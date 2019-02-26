e46a778b6e405e3868052c1a3c9ac439625a7a19
x87 <f0rest_x87@163.com>
2019-02-26 17:22:35 +0800

    添加开源协议信息

--- src/main/resources/webroot/ui/js/date.ext.js
+++ src/main/resources/webroot/ui/js/date.ext.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Yang Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 Date.prototype.format = function(format) {
     var o = {
         "M+": this.getMonth() + 1,
@@ -20,7 +36,7 @@ Date.prototype.format = function(format) {
 }
 
 /** 
- *转换日期对象为日期字符串 
+ * 转换日期对象为日期字符串 
  * @param date 日期对象 
  * @param isFull 是否为完整的日期数据, 
  * 为true时, 格式如"2000-03-05 01:05:04" 
@@ -38,7 +54,7 @@ function getSmpFormatDate(date, isFull) {
 }
 
 /** 
- *转换当前日期对象为日期字符串 
+ * 转换当前日期对象为日期字符串 
  * @param date 日期对象 
  * @param isFull 是否为完整的日期数据, 
  * 为true时, 格式如"2000-03-05 01:05:04" 
@@ -50,7 +66,7 @@ function getSmpFormatNowDate(isFull) {
 }
 
 /** 
- *转换long值为日期字符串 
+ * 转换long值为日期字符串 
  * @param l long值 
  * @param isFull 是否为完整的日期数据, 
  * 为true时, 格式如"2000-03-05 01:05:04" 
@@ -62,7 +78,7 @@ function getSmpFormatDateByLong(l, isFull) {
 }
 
 /** 
- *转换long值为日期字符串 
+ * 转换long值为日期字符串 
  * @param l long值 
  * @param pattern 格式字符串,例如：yyyy-MM-dd hh:mm:ss 
  * @return 符合要求的日期字符串 
@@ -72,7 +88,7 @@ function getFormatDateByLong(l, pattern) {
 }
 
 /** 
- *转换日期对象为日期字符串 
+ * 转换日期对象为日期字符串 
  * @param l long值 
  * @param pattern 格式字符串,例如：yyyy-MM-dd hh:mm:ss 
  * @return 符合要求的日期字符串 
