460d736245b3c94f695ea4fee787ac255151d835
x87 <f0rest_x87@163.com>
2019-03-08 19:04:27 +0800

    添加 JavaScript 方法

--- src/main/resources/webroot/static/components/common/string.js
+++ src/main/resources/webroot/static/components/common/string.js
@@ -1,11 +1,15 @@
-if (typeof String.prototype.startsWith != 'function') {
-    String.prototype.startsWith = function(prefix) {
-        return this.slice(0, prefix.length) === prefix;
-    };
+function stringFunctions() {
+    if (!String.prototype.startsWith || typeof String.prototype.startsWith != 'function') {
+        String.prototype.startsWith = function(prefix) {
+            return this.slice(0, prefix.length) === prefix;
+        };
+    }
+
+    if (!String.prototype.endsWith || typeof String.prototype.endsWith != 'function') {
+        String.prototype.endsWith = function(suffix) {
+            return this.indexOf(suffix, this.length - suffix.length) !== -1;
+        };
+    }
 }
 
-if (typeof String.prototype.endsWith != 'function') {
-    String.prototype.endsWith = function(suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
-}
+stringFunctions();
--- src/main/resources/webroot/ui/js/date.ext.js
+++ src/main/resources/webroot/ui/js/date.ext.js
@@ -14,25 +14,27 @@
  * limitations under the License.
  */
 
-Date.prototype.format = function(format) {
-    var o = {
-        "M+": this.getMonth() + 1,
-        "d+": this.getDate(),
-        "h+": this.getHours(),
-        "m+": this.getMinutes(),
-        "s+": this.getSeconds(),
-        "q+": Math.floor((this.getMonth() + 3) / 3),
-        "S": this.getMilliseconds()
-    }
-    if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    }
-    for (var k in o) {
-        if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+function dateFunctions() {
+    Date.prototype.format = function(format) {
+        var o = {
+            "M+": this.getMonth() + 1,
+            "d+": this.getDate(),
+            "h+": this.getHours(),
+            "m+": this.getMinutes(),
+            "s+": this.getSeconds(),
+            "q+": Math.floor((this.getMonth() + 3) / 3),
+            "S": this.getMilliseconds()
         }
+        if (/(y+)/.test(format)) {
+            format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+            }
+        }
+        return format;
     }
-    return format;
 }
 
 /** 
@@ -102,3 +104,5 @@ function getFormatDate(date, pattern) {
     }
     return date.format(pattern);
 }
+
+dateFunctions();
